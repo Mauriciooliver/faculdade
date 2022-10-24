4d6b280e93022e73b7c2f14585e55c3abd7759a1
Mauricio <101533776+Mauriciooliver@users.noreply.github.com>
2022-10-23 21:32:21 -0300

    up

--- estduos/def.py
+++ estduos/def.py
@@ -1,1 +1,35 @@
-def 
+'''def divisao(n1, n2):
+    return n1 ** n2
+
+
+potencia = divisao(20, 2)
+
+print(potencia)'''
+
+
+'''def divisao(n1, n2):
+    if n2 == 0:
+        return
+
+    return n1 / n2
+
+divide = divisao(9,3)
+
+if not divide:
+    print(divide)
+
+else:
+    print('valor invalido')'''
+
+
+def dumb():
+    return 1 , 'mauricio'
+
+print(dumb(), type(dumb()))
+
+
+
+
+
+
+
